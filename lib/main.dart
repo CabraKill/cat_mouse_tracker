57794c5ef8dca459b66682d8541da94adf3d2657
Raphael <raphaeldesouza@outlook.com>
2023-03-20 23:12:17 -0300

    remove old deploy action infra

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:cat_mouse_tracker/presentation/pages/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
